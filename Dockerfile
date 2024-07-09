e9d79455c2e0ae5df1118adb2386e21068297dd7
Doniyor <niazovd@gmail.com>
2024-07-09 07:30:01 -0700

    add dockerfile and keyvault support

--- Dockerfile
+++ Dockerfile
@@ -1,7 +1,6 @@
 # Use the appropriate base images for your application
 FROM --platform=linux/amd64 mcr.microsoft.com/dotnet/aspnet:8.0 AS base
 USER app
-ENV DOTNET_EnableWriteXorExecute=0
 WORKDIR /app
 EXPOSE 8080
 EXPOSE 8081
@@ -13,13 +12,12 @@ WORKDIR /src
 
 # Copy only the project file and restore dependencies
 COPY ["Temp/Temp.csproj", "Temp/"]
-RUN dotnet restore "./Temp/Temp.csproj" --verbosity detailed
+RUN dotnet restore "./Temp/Temp.csproj"
 
 # Copy the remaining source code and build the application
 COPY . .
 WORKDIR "/src/Temp"
 RUN dotnet build "./Temp.csproj" -c $BUILD_CONFIGURATION -o /app/build
-
 # Publish the application
 FROM build AS publish
 ARG BUILD_CONFIGURATION=Release
--- Temp/Program.cs
+++ Temp/Program.cs
@@ -1,4 +1,5 @@
 using Azure.Identity;
+using Azure.Security.KeyVault.Secrets;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -7,23 +8,37 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-//string appConfUri = 
-//string clientId = builder.Configuration["ManagedIdentityClientId"];
+string keyVaultUri = builder.Configuration["KeyVaultUri"];
+string appConfigUri = builder.Configuration["AppConfigUri"];
+string managedIdentityClientId = builder.Configuration["ManagedIdentityClientId"];
 
-string appConfigUri = "";
 var credentialOptions = new DefaultAzureCredentialOptions
 {
-	ManagedIdentityClientId = ""
+	ManagedIdentityClientId = managedIdentityClientId
 };
 
-builder.Configuration.AddAzureAppConfiguration(options =>
+System.Console.WriteLine($"AppConfigUri: {appConfigUri}");
+System.Console.WriteLine($"ManagedIdentityClientId: {managedIdentityClientId}");
+System.Console.WriteLine($"AppConfigUri: {keyVaultUri}");
+
+if (!string.IsNullOrEmpty(keyVaultUri))
 {
-	options.Connect(new Uri(appConfigUri), new DefaultAzureCredential(credentialOptions))
-					.ConfigureKeyVault(kv =>
-					{
-						kv.SetCredential(new DefaultAzureCredential(credentialOptions));
-					});
-});
+	builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential(credentialOptions));
+}
+
+if (!string.IsNullOrEmpty(appConfigUri) && !string.IsNullOrEmpty(managedIdentityClientId))
+{
+	builder.Configuration.AddAzureAppConfiguration(options =>
+	{
+		options.Connect(new Uri(appConfigUri), new ManagedIdentityCredential(managedIdentityClientId));
+
+		//	options.Connect(new Uri(appConfigUri), new DefaultAzureCredential(credentialOptions));
+		// .ConfigureKeyVault(kv =>
+		// {
+		// 	kv.SetCredential(new DefaultAzureCredential(credentialOptions));
+		// });
+	});
+}
 
 var app = builder.Build();
 
