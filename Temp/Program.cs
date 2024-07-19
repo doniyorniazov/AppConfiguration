a5d28ac1287751cd1456869ccaaa915b816ee07d
Doniyor <niazovd@gmail.com>
2024-07-18 22:29:41 -0700

    app config change

--- Temp/Program.cs
+++ Temp/Program.cs
@@ -1,44 +1,24 @@
 using Azure.Identity;
 using Azure.Security.KeyVault.Secrets;
+using Microsoft.Extensions.Azure;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-string keyVaultUri = builder.Configuration["KeyVaultUri"];
+string appConfigCS = builder.Configuration["AppConfigCS"];
 string appConfigUri = builder.Configuration["AppConfigUri"];
 string managedIdentityClientId = builder.Configuration["ManagedIdentityClientId"];
 
-var credentialOptions = new DefaultAzureCredentialOptions
-{
-	ManagedIdentityClientId = managedIdentityClientId
-};
+System.Console.WriteLine("AppConfigUri: " + appConfigUri);
+System.Console.WriteLine("ManagedIdentityClientId: " + managedIdentityClientId);
 
-System.Console.WriteLine($"AppConfigUri: {appConfigUri}");
-System.Console.WriteLine($"ManagedIdentityClientId: {managedIdentityClientId}");
-System.Console.WriteLine($"AppConfigUri: {keyVaultUri}");
-
-if (!string.IsNullOrEmpty(keyVaultUri))
-{
-	builder.Configuration.AddAzureKeyVault(new Uri(keyVaultUri), new DefaultAzureCredential(credentialOptions));
-}
-
-if (!string.IsNullOrEmpty(appConfigUri) && !string.IsNullOrEmpty(managedIdentityClientId))
-{
-	builder.Configuration.AddAzureAppConfiguration(options =>
-	{
-		options.Connect(new Uri(appConfigUri), new ManagedIdentityCredential(managedIdentityClientId));
-
-		//	options.Connect(new Uri(appConfigUri), new DefaultAzureCredential(credentialOptions));
-		// .ConfigureKeyVault(kv =>
-		// {
-		// 	kv.SetCredential(new DefaultAzureCredential(credentialOptions));
-		// });
-	});
-}
+builder.Configuration.AddAzureAppConfiguration(options =>
+		options.Connect(
+				new Uri(appConfigUri),
+				new ManagedIdentityCredential(managedIdentityClientId)));
 
 var app = builder.Build();
 
